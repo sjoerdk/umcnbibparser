90af23a13406cbd45be2eec4bc6173774d554db2
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2022-02-25 16:48:06 +0100

    Adds mysql rendering and template

--- bibparser/mysql.py
+++ bibparser/mysql.py
@@ -1,3 +1,46 @@
-"""Functions to write bibfile contents to MYSQL file
+"""Functions to write bibfile contents to MYSQL file.
 
+Notes
+-----
+Functions here are not meant to be generic. Mysql db format is specific to the
+radboud research websites db
 """
+from typing import Dict
+
+from jinja2.environment import Environment
+from jinja2.loaders import PackageLoader
+
+GENERIC_ENTRY_TYPES = {"article": "1_Article",
+                       "inproceedings": "2_Inproceedings",
+                       "abstract": "3_Abstract",
+                       "phdthesis": "4_PHDThesis",
+                       "book": "",
+                       "incollection": ""}
+
+
+def enrich_parsed(parsed_bib: Dict) -> Dict:
+    """Add legacy items to each bib entry needed in db for historical reasons
+
+    Notes
+    -----
+    Modifies input dict
+    """
+    for key in parsed_bib.keys():
+        try:
+            parsed_bib[key]['genericentrytype'] = \
+                GENERIC_ENTRY_TYPES[parsed_bib[key]['type'].lower()]
+        except KeyError as e:
+            raise(KeyError(f"Error in bib entry '{key}'")) from e
+
+    return parsed_bib
+
+
+def to_research_db_mysql(enriched_bib) -> str:
+    """Render parsed and enriched bib file to a mysql command that will create
+    the research db research from scratch
+    """
+
+    loader = PackageLoader(package_name="bibparser", package_path="templates")
+    template_env = Environment(loader=loader, autoescape=True)
+    template = template_env.get_template("newpubs.sql.j2")
+    return template.render(entries=enriched_bib)
--- tests/test_bibparser.py
+++ tests/test_bibparser.py
@@ -1,5 +1,15 @@
+import pytest as pytest
+
 from bibparser.bibreader import parse_bibtex_file
+from bibparser.mysql import enrich_parsed, to_research_db_mysql
 from tests import RESOURCE_PATH
+
+
+@pytest.fixture
+def a_parsed_bibfile():
+    """A parsed bib file with 100 entries"""
+    return parse_bibtex_file(filename=RESOURCE_PATH / 'some_entries.bib',
+                             full_strings_bib=RESOURCE_PATH / 'fullstrings.bib')
 
 
 def test_parse_bib():
@@ -11,6 +21,18 @@ def test_parse_bib():
     assert len(bib_items) == 100
 
 
+def test_mysql_enrich(a_parsed_bibfile):
+    for item in a_parsed_bibfile.values():
+        assert 'genericentrytype' not in item
+
+    enriched = enrich_parsed(a_parsed_bibfile)
+    for item in enriched.values():
+        assert 'genericentrytype' in item
 
 
+def test_to_mysql_db_file(a_parsed_bibfile):
+    """Render a bibfile to a mysql file that creates the bibfiles content"""
+    mysql = to_research_db_mysql(enrich_parsed(a_parsed_bibfile))
 
+    assert mysql.count('INSERT') == 100  # one insert for each item
+    assert mysql.count('{{') == 0  # no jinja tags unreplaced
